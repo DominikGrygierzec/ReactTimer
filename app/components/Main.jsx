e57648cd3f1064b9e5f5e2c2815a2c17424f4041
Dominik Grygierzec <dominikgrygierzec@gmx.de>
2017-03-15 11:35:33 +0100

    Add automatic tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,8 +1,10 @@
 var React = require('react');
+var Navigation = require('Navigation');
 
 var Main = (props) => {
   return (
     <div>
+      <Navigation />
       <div>
         <div>
           <p>Main.jsx Rendered</p>
